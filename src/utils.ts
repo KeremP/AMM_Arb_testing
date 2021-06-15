97fc33599edd0a9cfc2eab0b72e5c635961e74f9
KeremP <kerpro26@gmail.com>
2021-06-14 21:04:32 -0400

    flashbots relay singer key check

--- src/utils.ts
+++ src/utils.ts
@@ -13,7 +13,7 @@ return value.mul(10000).div(divisor).toNumber() / 10000
 }
 
 
-//used to sign FlashBot bundles - should not be actualy trader wallet address
+//used to sign FlashBot bundles - should not be actual trader wallet address
 //this is an arbitary wallet to sign sequential transactions in bundle
 export function getDefaultRelaySigningKey(): string {
 console.warn("You have not specified an explicity FLASHBOTS_RELAY_SIGNING_KEY environment variable. Creating random signing key, this searcher will not be building a reputation for next run")
